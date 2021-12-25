4d2f9c26dbbe65e75ceae798ca018bb5cd03ab8a
kaniel-outis <36097092+kaniel-outis@users.noreply.github.com>
2021-12-26 00:01:55 +0800

    Delete bustub-master-2020 directory

--- bustub-master-2020/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /bustub
-# Build directory: /bustub/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest-build")
-subdirs("src")
-subdirs("test")
